5313ef4ca5c1a3bc7b84e57636081a352cc8bb90
Mostary Jahan <mostaryjahan01@gmail.com>
2024-09-30 23:22:20 +0600

    added nav

---
+++ app/about/mission/page.jsx
@@ -0,0 +1,10 @@
+
+const page = () => {
+    return (
+        <div>
+            This is mission
+        </div>
+    );
+};
+
+export default page;
---
+++ app/about/page.jsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const page = () => {
+    return (
+        <div className="mt-10 bg-gray-400 p-16">
+            <h1>This is about</h1>
+            <h2>Go to Mission page</h2>
+            <p><Link href='/about/mission'>Click here</Link></p>
+            
+        </div>
+    );
+};
+
+export default page;
